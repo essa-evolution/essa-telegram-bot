2a0c6e912fd62a2828aeca3a803bf8c847a51f3e
essa-evolution <essaevolution@gmail.com>
2026-05-11 06:29:16 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1471,7 +1471,7 @@ if (voice) {
 
   await axios.post(
    
-    `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendAudio`,
+    `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendVoice`,
     form,
     {
       headers: form.getHeaders ? form.getHeaders() : {}
