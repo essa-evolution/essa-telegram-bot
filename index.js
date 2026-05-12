8f5b0528dccb6522ad60fb7c53dc5abdeeebd462
essa-evolution <essaevolution@gmail.com>
2026-05-12 23:17:12 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1478,7 +1478,7 @@ const mode = detectMode(userText);
   if (userSessions[chatId].length > 10) {
     userSessions[chatId] = userSessions[chatId].slice(-10);
   }
-},
+}
 
   try {
     const aiResponse = await axios.post(
